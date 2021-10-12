030cbaf4ffe258a00ad2517a3ffab2c5e25fdd49
cws0316 <dev.chwonseok@gmail.com>
2021-10-13 01:29:33 +0900

    Done with the practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import ContactUs from './components/pages/ContactUs';
+import SignUp from './components/pages/SignUp';
+import Home from './components/pages/Home';
+import Marketing from './components/pages/Marketing';
+import Consulting from './components/pages/Consulting';
+
 import './App.css';
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/services" exact component={Services} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/contact-us" exact component={ContactUs} />
+        <Route path="/sign-up" exact component={SignUp} />
+        <Route path="/marketing" exact component={Marketing} />
+        <Route path="/consulting" exact component={Consulting} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/pages/Consulting.js
@@ -0,0 +1,7 @@
+export default function Consulting() {
+  return (
+    <>
+      <h1 className="consulting">CONSULTING</h1>
+    </>
+  );
+}
