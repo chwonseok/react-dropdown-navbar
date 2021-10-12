b871e64872f84cc564f9e26c0e64a3e8695282ac
cws0316 <dev.chwonseok@gmail.com>
2021-10-12 21:37:52 +0900

    Installed react-router-dom

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
+import Dropdown from './Dropdown';
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="navbar-logo"></Link>
+      </nav>
+    </>
+  );
+}
